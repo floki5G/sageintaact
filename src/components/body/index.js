62b1d532d38640ffb2b2c2e45e11865d20f63098
sameer kumar verma <83910505+floki5G@users.noreply.github.com>
2022-01-13 19:31:55 +0530

    Add files via upload

---
+++ src/components/body/index.js
@@ -0,0 +1,147 @@
+import React, { useState } from 'react'
+import './index.css'
+
+export const Body = () => {
+
+
+    return (
+        <>
+<div class="loader"></div>
+            <div className='body-h1'>
+                <div>
+                    <div className='h1 '>
+                        The Leader in<br></br>Accounting and Financial <br></br>Management Software
+                    </div>
+                    <br></br>
+
+                    <div className='h6'>
+                        Whether you’re a growing startup or an established public company,<br></br> we’re the right partner for your long-term success.
+                    </div>
+                    <br></br>
+                    <br></br>
+
+                    <span className='h4 border border-success p-1' >Watch demo</span>
+                </div>
+
+                <div >
+                    <img src="https://www.sageintacct.com/sites/default/files/accounting-financial-management-software-header.svg" alt='something went wrong'></img>
+
+                </div>
+
+            </div>
+            <div className='body'>
+
+                <div className='body-h2'>
+
+                    <div className='my-5 mx-4' style={{ borderRight: "2px solid black" }}>
+                        <div className='h4 bodyheadingh4'>See Sage Intacct in action </div>
+                        <div className='bodyheadingdesbc'>Learn what Sage Intacct can offer <br></br>you with a live demo followed<br></br> by Q&A.</div>
+                        <div className='text-success h6  mt-3' >Sign up now <i class="fas fa-arrow-right"></i></div>
+                    </div>
+
+
+
+                    <div className='my-5 mx-4' style={{ borderRight: "2px solid black" }}>
+                        <div className='h4 bodyheadingh4'>    #1 for the 5th year in a row  </div>
+                        <div className='bodyheadingdesbc'> Once again, Sage Intacct received<br></br> the highest product score in core <br></br>financials for the lower <br></br>midsize enterprises use case<br></br> from Gartner1.  </div>
+                        <div className='text-success h6  mt-3'>   Read report  <i class="fas fa-arrow-right"></i></div>
+
+                    </div>
+
+                    <div className='my-5 mx-4'>
+                        <div className='h4 bodyheadingh4' > Outgrowing QuickBooks? </div>
+                        <div className='bodyheadingdesbc'>Find out why thriving<br></br> organizations say goodbye to<br></br> QuickBooks and partner with<br></br> Sage Intacct to power their<br></br> growth.</div>
+                        <div className='text-success h6 mt-3'>Learn why  <i class="fas fa-arrow-right"></i></div>
+                    </div>
+
+                </div>
+
+                <div className='body-h3'>
+
+                    <div className='bodyheading center'>
+                        <h1>Thrive in today’s ever-changing,  digital world</h1><h4> with connected solutions that simply work</h4>
+                    </div>
+                    <div className='item ' >
+                        <div className='center'   >
+                            <img src="https://www.sageintacct.com/sites/default/files/intelligent-organization.svg"></img>
+                        </div>
+                        <div className='item-right'>
+
+                            <div >
+                                <img src="" />
+                                Financials
+
+
+                            </div>
+
+                            <div>
+                                <img src="" />
+
+                                Planning</div>
+
+                            <div>
+                                <img src="" />
+
+                                Analytics</div>
+
+                            <div>
+                                <img src="" />
+
+                                HR & Payroll</div>
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+
+
+
+                <div className='body-h4'>
+
+                    <div className='bodyheading center'>
+                        <h1>Solutions for your industry’s</h1>
+                        <h3> specific needs</h3>
+                    </div>
+                    <div className='industery ' >
+                        <div className='center industery-left'   >
+                            <div>For Any Industry</div>
+
+                            <div>Construction and Real Estate</div>
+
+                            <div>Financial Services</div>
+
+                            <div>HR & Payroll</div>
+                            <div>Healthcare</div>
+                            <div>Nonprofits</div>
+
+                            <div>Professional  Services</div>
+
+
+                            <div>Subscription and SaaS</div>
+
+
+                            <div>Wholesale  Distribution</div>
+                        </div>
+                        <div className='center'>
+                            <div>
+                                <h2>For Any Industry</h2>
+                                <h5> The smart cloud accounting solution to unleash the potential of your business. Learn why industry leaders choose Sage Intacct—rated #1 in customer satisfaction by G2.</h5></div>
+
+                            <img src="https://www.sageintacct.com/sites/default/files/styles/home-industry/public/accounting-financial-management-for-professional-services.png?itok=wD8WXM73"></img>
+
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </>
+    )
+}
